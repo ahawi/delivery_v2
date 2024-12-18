48138a36ae715e67fdce99ff7b9a38a66863d680
ultrasue <nastya.murlla@gmail.com>
2024-12-18 12:42:44 +0300

    add intermediate results

--- js/main.js
+++ js/main.js
@@ -1,243 +1,245 @@
-// const cardsFunc = () => {
-//   const container = document.querySelector(".col-md-8");
+const cards = [
+  {
+    id: 0,
+    name: "Калифорния",
+    image: "california-hit.jpg",
+    price: 300,
+    weight: 180,
+  },
+  {
+    id: 1,
+    name: "Калифорния темпура",
+    image: "california-tempura.jpg",
+    price: 250,
+    weight: 205,
+  },
+  {
+    id: 2,
+    name: 'Запеченный ролл "Калифорния"',
+    image: "zapech-california.jpg",
+    price: 230,
+    weight: 182,
+  },
+  {
+    id: 3,
+    name: "Филадельфия",
+    image: "philadelphia.jpg",
+    price: 320,
+    weight: 230,
+  },
+];
 
-//   const cards = [
-//     {
-//       id: 0,
-//       name: "Калифорния",
-//       quantity: 6,
-//       image: "california-hit.jpg",
-//       price: 300,
-//       weight: 180,
-//     },
-//     {
-//       id: 1,
-//       name: "Калифорния темпура",
-//       quantity: 6,
-//       image: "california-tempura.jpg",
-//       price: 250,
-//       weight: 205,
-//     },
-//     {
-//       id: 2,
-//       name: 'Запеченный ролл "Калифорния"',
-//       quantity: 6,
-//       image: "zapech-california.jpg",
-//       price: 230,
-//       weight: 182,
-//     },
-//     {
-//       id: 3,
-//       name: "Филадельфия",
-//       quantity: 6,
-//       image: "philadelphia.jpg",
-//       price: 320,
-//       weight: 230,
-//     },
-//   ];
+const renderCards = (array) => {
+  const container = document.querySelector(".sushi__list");
+  container.innerHTML = "";
 
-//   const renderCards = (array) => {
-//     container.innerHTML = "";
+  array.forEach((card) => {
+    container.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li class="sushi__item">
+        <div class="sushi__item-img">
+          <img src="./img/roll/${card.image}" alt="" width="265" height="265" loading="lazy" />
+        </div>
+        <h2 class="sushi__item-title">${card.name}</h2>
+        <div class="sushi__bottom">
+          <div class="sushi__bottom-right-block">
+            <span class="sushi__bottom-right-block-weight">${card.weight}г.</span>
+            <span class="sushi__bottom-right-block-price">${card.price} ₽</span>
+          </div>
+        </div>
+        <div class="sushi__bottom-button">
+          <button data-js-add-button class="sushi__button">+ в корзину</button>
+        </div>
+      </li>`
+    );
 
-//     array.forEach((card) => {
-//       container.insertAdjacentHTML(
-//         "beforeend",
-//         `<div class="col-md-6">
-//               <div class="card mb-4">
-//               <img
-//                   class="product-img"
-//                   src="img/roll/${card.image}"
-//                   alt=""
-//               />
-//               <div class="card-body text-center">
-//                   <h4 class="item-title">${card.name}</h4>
-//                   <p><small class="text-muted">${card.quantity} шт.</small></p>
+    // Привязываем обработчики событий к текущей карточке
+    cardControls(container.lastElementChild, card);
+  });
+};
 
-//                   <div class="details-wrapper">
-//                   <div class="items">
-//                       <div data-js-button-minus class="items__control">-</div>
-//                       <div data-js-count class="items__current">1</div>
-//                       <div data-js-button-plus class="items__control">+</div>
-//                   </div>
+const cardControls = (cardElement, card) => {
+  const addButton = cardElement.querySelector("[data-js-add-button]");
 
-//                   <div class="price">
-//                       <div class="price__weight">${card.weight}г.</div>
-//                       <div class="price__currency">${card.price} ₽</div>
-//                   </div>
-//                   </div>
+  addButton.addEventListener("click", () => {
+    addToCart(card);
+  });
+};
 
-//                   <button
-//                   data-js-add-button
-//                   type="button"
-//                   class="btn btn-block btn-outline-warning"
-//                   >
-//                   + в корзину
-//                   </button>
-//               </div>
-//               </div>
-//           </div>`
-//       );
-//     });
+const addToCart = (card) => {
+  const cartEmpty = document.querySelector(".cart__list");
 
-//     addToCart();
-//   };
+  const cartAlert = document.querySelector(".cart__alert");
+  const addButton = document.querySelector(
+    `[data-js-add-button][data-card-id="${card.id}"]`
+  );
 
-//   renderCards(cards);
-// };
+  let quantity = 1;
 
-// const addToCart = () => {
-//   const buttonPlus = document.querySelector("[data-js-button-plus]");
-//   const buttonMinus = document.querySelector("[data-js-button-minus]");
-//   const currentDisplay = document.querySelector("[data-js-count]");
-//   const cartWrapper = document.querySelector(".cart-wrapper");
-//   const addButton = document.querySelector("[data-js-add-button]");
-//   const emptyCart = document.querySelector("[data-js-empty-cart]");
+  if (cartAlert) cartAlert.remove();
 
-//   let currentCount = 1;
+  const existingCartItem = Array.from(cartEmpty.children).find((item) => {
+    return item.querySelector(".cart__item-title").textContent === card.name;
+  });
 
-//   currentDisplay.textContent = currentCount;
+  if (existingCartItem) {
+    addButton.disabled = true;
+    return;
+  } else {
+    cartEmpty.insertAdjacentHTML(
+      "afterbegin",
+      `
+      <li class="cart__item">
+        <img class="cart__item-img" src="./img/roll/${card.image}" alt="" />
+        <div class="cart__item-description">
+          <h4 class="cart__item-title">${card.name}</h4>
+          <span class="cart__item-weight">${quantity} шт. / ${
+        card.weight * quantity
+      }г.</span>
+          <div class="cart__bottom">
+            <div class="cart__bottom-counter">
+              <button class="cart__bottom-counter-item" type="button" data-js-cart-button-minus>-</button>
+              <div class="cart__bottom-counter-count" data-js-cart-count>${quantity}</div>
+              <button class="cart__bottom-counter-item" type="button" data-js-cart-button-plus>+</button>
+            </div>
+            <span class="cart__bottom-right-block-price">${
+              card.price * quantity
+            } ₽</span>
+          </div>
+        </div>
+      </li>`
+    );
 
-//   document.addEventListener("click", (event) => {
-//     if (event.target == buttonMinus) {
-//       if (currentCount > 1) {
-//         currentCount--;
-//         currentDisplay.textContent = currentCount;
-//       }
-//     }
+    // Привязываем обработчики событий к кнопкам в корзине
+    cartControls(cartEmpty.firstElementChild, card);
+    updateTotalPrice();
+  }
+};
 
-//     if (event.target == buttonPlus) {
-//       if (currentCount < 99) {
-//         currentCount++;
-//         currentDisplay.textContent = currentCount;
-//       }
-//     }
+const cartControls = (cartItemElement, card) => {
+  const cartButtonPlus = cartItemElement.querySelector(
+    "[data-js-cart-button-plus]"
+  );
+  const cartButtonMinus = cartItemElement.querySelector(
+    "[data-js-cart-button-minus]"
+  );
+  const cartCounterDisplay = cartItemElement.querySelector(
+    "[data-js-cart-count]"
+  );
 
-//     if (event.target == addButton) {
-//       emptyCart.innerHTML = "";
-//       cartWrapper.insertAdjacentHTML(
-//         "afterbegin",
-//         `<div class="cart-item">
-//               <div class="cart-item__top">
-//                   <div class="cart-item__img">
-//                   <img src="img/roll/philadelphia.jpg" alt="" />
-//                   </div>
-//                   <div class="cart-item__desc">
-//                   <div class="cart-item__title">Филадельфия хит ролл</div>
-//                   <div class="cart-item__weight">6 шт. / 182г.</div>
+  showTotal();
 
-//                   <div class="cart-item__details">
-//                       <div class="items items--small">
-//                       <div class="items__control">-</div>
-//                       <div class="items__current">${currentCount}</div>
-//                       <div class="items__control">+</div>
-//                       </div>
+  cartButtonMinus.addEventListener("click", () => {
+    let countInCart = Number(cartCounterDisplay.textContent);
 
-//                       <div class="price">
-//                       <div class="price__currency">230 ₽</div>
-//                       </div>
-//                   </div>
-//                   </div>
-//               </div>
-//               </div>`
-//       );
-//     }
-//   });
-// };
-
-// addToCart();
-
-const cardsFunc = () => {
-  const container = document.querySelector(".grid");
-
-  const cards = [
-    {
-      id: 0,
-      name: "Калифорния",
-      quantity: 6,
-      image: "california-hit.jpg",
-      price: 300,
-      weight: 180,
-    },
-    {
-      id: 1,
-      name: "Калифорния темпура",
-      quantity: 6,
-      image: "california-tempura.jpg",
-      price: 250,
-      weight: 205,
-    },
-    {
-      id: 2,
-      name: 'Запеченный ролл "Калифорния"',
-      quantity: 6,
-      image: "zapech-california.jpg",
-      price: 230,
-      weight: 182,
-    },
-    {
-      id: 3,
-      name: "Филадельфия",
-      quantity: 6,
-      image: "philadelphia.jpg",
-      price: 320,
-      weight: 230,
-    },
-  ];
-
-  const renderCards = (array) => {
-    container.innerHTML = "";
-
-    array.forEach((card) => {
-      container.insertAdjacentHTML(
-        "beforeend",
-        `<div class="col-md-6">
-                <div class="card mb-4">
-                <img
-                    class="product-img"
-                    src="img/roll/${card.image}"
-                    alt=""
-                />
-                <div class="card-body text-center">
-                    <h4 class="item-title">${card.name}</h4>
-                    <p><small class="text-muted">${card.quantity} шт.</small></p>
-    
-                    <div class="details-wrapper">
-                    <div class="items">
-                        <div data-js-button-minus class="items__control">-</div>
-                        <div data-js-count class="items__current">1</div>
-                        <div data-js-button-plus class="items__control">+</div>
-                    </div>
-    
-                    <div class="price">
-                        <div class="price__weight">${card.weight}г.</div>
-                        <div class="price__currency">${card.price} ₽</div>
-                    </div>
-                    </div>
-    
-                    <button
-                    data-js-add-button
-                    type="button"
-                    class="btn btn-block btn-outline-warning"
-                    >
-                    + в корзину
-                    </button>
-                </div>
-                </div>
-            </div>`
+    if (countInCart > 0) {
+      countInCart -= 1;
+      cartCounterDisplay.textContent = countInCart;
+      updateCartCharacters(
+        cartItemElement,
+        card.price,
+        countInCart,
+        card.weight,
+        card.quantity
       );
-    });
+    }
 
-    addToCart();
-  };
+    if (countInCart === 0) {
+      cartItemElement.remove();
+    }
 
-  renderCards(cards);
+    if (document.querySelectorAll(".cart__item").length == 0) {
+      hideTotal();
+      showCartAlert();
+    }
+    updateTotalPrice();
+  });
+
+  cartButtonPlus.addEventListener("click", () => {
+    let countInCart = Number(cartCounterDisplay.textContent);
+
+    countInCart += 1;
+    cartCounterDisplay.textContent = countInCart;
+
+    updateCartCharacters(cartItemElement, card.price, countInCart, card.weight);
+    updateTotalPrice();
+  });
 };
 
-const addToCart = () => {
-  const cartWrapper = document.querySelector(".cart-wrapper");
-  const emptyCart = document.querySelector("[data-js-empty-cart]");
+const updateCartCharacters = (
+  cartItemElement,
+  pricePerUnit,
+  quantity,
+  weight
+) => {
+  const cartPrice = cartItemElement.querySelector(
+    ".cart__bottom-right-block-price"
+  );
+  const cartQuantity = cartItemElement.querySelector(".cart__item-weight");
 
-
+  cartPrice.textContent = `${pricePerUnit * quantity} ₽`;
+  cartQuantity.textContent = `${quantity} шт. / ${weight * quantity}г.`;
 };
 
-cardsFunc();
+const updateCartItemCount = (card, currentCount) => {
+  const cartItems = document.querySelectorAll(".cart__item");
+
+  cartItems.forEach((cartItem) => {
+    const cartTitle = cartItem.querySelector(".cart__item-title").textContent;
+
+    if (cartTitle === card.name) {
+      const cartCounterDisplay = cartItem.querySelector("[data-js-cart-count]");
+      const cartPriceDisplay = cartItem.querySelector(
+        ".cart__bottom-right-block-price"
+      );
+
+      cartCounterDisplay.textContent = currentCount;
+
+      const pricePerUnit = card.price;
+      cartPriceDisplay.textContent = `${pricePerUnit * currentCount} ₽`;
+    }
+  });
+};
+
+const showCartAlert = () => {
+  const alertContainer = document.querySelector(".cart__body");
+  if (!document.querySelector(".cart__alert")) {
+    alertContainer.insertAdjacentHTML(
+      "beforeend",
+      `<p class="cart__alert">Корзина пуста</p>
+       <ul class="cart__list"></ul>`
+    );
+  }
+};
+
+const showTotal = () => {
+  const totalPriceDisplay = document.querySelector(".cart__total");
+  if (totalPriceDisplay) {
+    totalPriceDisplay.style.display = "block"; // Скрываем элемент с общей суммой
+  }
+};
+
+const hideTotal = () => {
+  const totalPriceDisplay = document.querySelector(".cart__total");
+  if (totalPriceDisplay) {
+    totalPriceDisplay.style.display = "none"; // Скрываем элемент с общей суммой
+  }
+};
+
+const updateTotalPrice = () => {
+  const cartItem = document.querySelectorAll(".cart__item");
+  let totalPrice = 0;
+
+  cartItem.forEach((item) => {
+    const priceDisplay = item.querySelector(".cart__bottom-right-block-price");
+    totalPrice += Number(priceDisplay.textContent.replace(" ₽", ""));
+
+    const totalPriceDisplay = document.querySelector(
+      ".cart__total-title--orange"
+    );
+    totalPriceDisplay.textContent = `${totalPrice} ₽`;
+  });
+};
+
+renderCards(cards);
